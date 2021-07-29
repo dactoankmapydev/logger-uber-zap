23c441bad9bb1d1f5612266205e55dd17b21429e
dactoankmapydev <nguyendactoankma@gmail.com>
2021-07-29 08:44:14 +0700

    logger using uber zap

---
+++ go.mod
@@ -0,0 +1,8 @@
+module zap-log
+
+go 1.16
+
+require (
+	github.com/natefinch/lumberjack v2.0.0+incompatible
+	go.uber.org/zap v1.18.1
+)
---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"os"
+	"time"
+
+	"github.com/natefinch/lumberjack"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+func main() {
+	logger := WriteLog()
+
+	logger.Debug("i am debug", zap.String("key", "debug"))
+	logger.Info("i am info", zap.String("key", "info"))
+	logger.Error("i am error", zap.String("key", "error"))
+}
+
+func getEncoder() zapcore.Encoder {
+	return zapcore.NewConsoleEncoder(zapcore.EncoderConfig{
+		MessageKey:   "message",
+		TimeKey:      "time",
+		LevelKey:     "level",
+		CallerKey:    "caller",
+		EncodeLevel:  CustomLevelEncoder,         //Format cách hiển thị level log
+		EncodeTime:   SyslogTimeEncoder,          //Format hiển thị thời điểm log
+		EncodeCaller: zapcore.ShortCallerEncoder, //Format dòng code bắt đầu log
+	})
+}
+
+func SyslogTimeEncoder(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString(t.Format("2006-01-02 15:04:05"))
+}
+
+func CustomLevelEncoder(level zapcore.Level, enc zapcore.PrimitiveArrayEncoder) {
+	enc.AppendString("[" + level.CapitalString() + "]")
+}
+
+func logErrorWriter() zapcore.WriteSyncer {
+	errFileLog, _ := os.OpenFile("./error.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	return zapcore.NewMultiWriteSyncer(
+		zapcore.AddSync(&lumberjack.Logger{
+			Filename: errFileLog.Name(),
+			MaxSize:  500, // megabytes
+			MaxAge:   30,  // days
+		}),
+		zapcore.AddSync(os.Stdout))
+}
+
+func logInfoWriter() zapcore.WriteSyncer {
+	errFileLog, _ := os.OpenFile("./info.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	return zapcore.NewMultiWriteSyncer(
+		zapcore.AddSync(&lumberjack.Logger{
+			Filename: errFileLog.Name(),
+			MaxSize:  500, // megabytes
+			MaxAge:   30,  // days
+		}),
+		zapcore.AddSync(os.Stdout))
+}
+
+func logDebugWriter() zapcore.WriteSyncer {
+	errFileLog, _ := os.OpenFile("./debug.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	return zapcore.NewMultiWriteSyncer(
+		zapcore.AddSync(&lumberjack.Logger{
+			Filename: errFileLog.Name(),
+			MaxSize:  500, // megabytes
+			MaxAge:   30,  // days
+		}),
+		zapcore.AddSync(os.Stdout))
+}
+
+// Write log to file by level log and console
+func WriteLog() *zap.Logger {
+	highWriteSyncer := logErrorWriter()
+	averageWriteSyncer := logDebugWriter()
+	lowWriteSyncer := logInfoWriter()
+
+	encoder := getEncoder()
+
+	highPriority := zap.LevelEnablerFunc(func(lev zapcore.Level) bool {
+		return lev >= zap.ErrorLevel
+	})
+
+	lowPriority := zap.LevelEnablerFunc(func(lev zapcore.Level) bool {
+		return lev < zap.ErrorLevel && lev > zap.DebugLevel
+	})
+
+	averagePriority := zap.LevelEnablerFunc(func(lev zapcore.Level) bool {
+		return lev < zap.ErrorLevel && lev < zap.InfoLevel
+	})
+
+	lowCore := zapcore.NewCore(encoder, lowWriteSyncer, lowPriority)
+	averageCore := zapcore.NewCore(encoder, averageWriteSyncer, averagePriority)
+	highCore := zapcore.NewCore(encoder, highWriteSyncer, highPriority)
+
+	logger := zap.New(zapcore.NewTee(lowCore, averageCore, highCore), zap.AddCaller())
+	return logger
+}
